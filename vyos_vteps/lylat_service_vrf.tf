4c9bc72970fc9a53aa5c1eb56002f4ac2f6e5575
ryanyweide@gmail.com <ryanyweide@gmail.com>
2026-04-25 06:00:45 -0400

    adding more svis/bridge domains

--- vyos_vteps/lylat_service_vrf.tf
+++ vyos_vteps/lylat_service_vrf.tf
@@ -4,33 +4,6 @@ resource "vyos_vrf_name" "lylat_service" {
 }
 
 
-resource "vyos_interfaces_bridge" "vxlan_bridge" {
-  depends_on = [vyos_interfaces_vxlan.svd_vxlan_intf]
-  identifier = {bridge = "br0"}
-  enable_vlan = true
-  mtu = "9169"
-}
-
-resource "vyos_interfaces_bridge_member_interface" "br0_vxlan0" {
-  depends_on = [vyos_interfaces_bridge.vxlan_bridge]
-  identifier = {
-    bridge = "br0"
-    interface = "vxlan0"
-  }
-}
-
-resource "vyos_interfaces_bridge_member_interface" "br0_eth3" {
-  depends_on = [vyos_interfaces_bridge_member_interface.br0_vxlan0]
-  identifier = {
-    bridge = "br0"
-    interface = "eth3"
-  }
-  allowed_vlan = [
-    "6",
-    "80",
-  ]
-}
-
 resource "vyos_interfaces_bridge_vif" "br0_vif_9006_anycast_gateway" {
   depends_on = [vyos_interfaces_bridge_member_interface.br0_eth3]
   identifier = {
