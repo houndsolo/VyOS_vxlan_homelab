8fa9c54a007bb5c983f8d7f536e9edac556d491b
ryanyweide@gmail.com <ryanyweide@gmail.com>
2026-05-26 20:21:32 -0400

    rename

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ module "configure_fabric" {
   vyos_key = var.vyos_key
 }
 
-module "create_vtep_vms" {
-  source = "./create_vtep_vms"
+module "create_fabric_vms" {
+  source = "./create_fabric_vms"
 
   fabric        = var.fabric
   pve_api_token = var.pve_api_token
